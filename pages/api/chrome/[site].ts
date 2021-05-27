abb8185159eb92747a3f3d0bd686a21dfd369b5c
Jack Hedaya <jackehedaya@gmail.com>
2021-05-27 07:47:20 -0400

    Made dynamic and added TS

--- pages/api/chrome/[site].ts
+++ pages/api/chrome/[site].ts
@@ -1,18 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
 const playwright = require("playwright-aws-lambda");
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   let browser = null;
 
+  console.log(playwright)
+
   try {
-    const browser = await playwright.launchChromium();
+    const browser = await playwright.launchChromium({ headless: true });
     const context = await browser.newContext();
 
     const page = await context.newPage();
-    await page.goto("https://google.com");
+    await page.goto(`https://${req.query.site || "google"}.com`);
 
     res.status(200).json(await page.title());
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(200).json(error);
   } finally {
     if (browser) {
