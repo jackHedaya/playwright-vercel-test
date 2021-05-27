25dae53ba629072c3458f32fadf78f864550110c
Jack Hedaya <jackehedaya@gmail.com>
2021-05-27 07:33:17 -0400

    Add link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Hit <code>/api/chrome</code> to test!</h1>
+      <h1 className={styles.title}>
+        <a href="/api/chrome">
+          Hit <code>/api/chrome</code> to test!
+        </a>
+      </h1>
     </div>
-  )
+  );
 }
