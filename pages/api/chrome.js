5ef7f43a34c133256766d60ae9f3979ef785c463
Jack Hedaya <jackehedaya@gmail.com>
2021-05-27 07:30:40 -0400

    Fix?

--- pages/api/chrome.js
+++ pages/api/chrome.js
@@ -8,10 +8,11 @@ export default async (req, res) => {
     const context = await browser.newContext();
 
     const page = await context.newPage();
-    await page.goto(event.url || "https://example.com");
+    await page.goto("https://google.com");
 
     res.status(200).json(await page.title());
   } catch (error) {
+    console.log(error)
     res.status(200).json(error);
   } finally {
     if (browser) {
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 style={styles.title}>Hit <code>/api/chrome</code> to test!</h1>
+      <h1 className={styles.title}>Hit <code>/api/chrome</code> to test!</h1>
     </div>
   )
 }
